b65898fc757f17ad2205bce8f48b22b81c068df5
drmarkreuter <mark.reuter@googlemail.com>
2022-07-10 01:32:33 +0100

    adding original comps

--- MusicBoard/app.R
+++ MusicBoard/app.R
@@ -3,8 +3,8 @@ library(shinythemes)
 
 # Define UI for application that draws a histogram
 ui <- navbarPage(theme = shinytheme("slate"),
-                 "Musicboard - A Ring Around the Moon",
-                 tabPanel("Library",
+                 HTML("<h2>Musicboard - Ring Around the Moon</h2>"),
+                 tabPanel(HTML("<h3>Library</h3>"),
                           tabsetPanel(type = "tabs",
                                       tabPanel(tags$h3("World Music"),
                                                fluidRow(
@@ -75,13 +75,52 @@ ui <- navbarPage(theme = shinytheme("slate"),
                                                uiOutput("ganstaParadise"),
                                                hr(),
                                                
+                                               ),
+                                      
+                                      tabPanel(tags$h3("YouTube - assorted"),
+                                               HTML("<h3>Django Reinhardt - The Best Of Django Reinhardt</h3>"),
+                                               uiOutput("django1"),
+                                               hr(),
+                                               
+                                               HTML("<h3>The Best of Tango</h3>"),
+                                               uiOutput("bestTango"),
+                                               hr(),
+                                               
+                                               HTML("<h3>Tango in Paris - Romantic French Music - French Tango Accordion Instrumental Music</h3>"),
+                                               uiOutput("tangoParis"),
+                                               hr(),
+                                               
+                                               HTML("<h3>Romantic French Tango Instrumental Music</h3>"),
+                                               uiOutput("romanticFench"),
+                                               hr(),
+                                               
+                                               HTML("<h3>Naked Tango - Slaughterhouse Dance Scene</h3>"),
+                                               uiOutput("nakedTango"),
+                                               hr(),
+                                               HTML('<a href="https://www.imdb.com/title/tt0100222/" target="_blank">Naked Tango on IMDB</a>'),
+                                               
+                                               HTML("<h3>Gotan Project - La Revancha Del Tango (Full Album)</h3>"),
+                                               uiOutput("gotan"),
+                                               hr(),
                                                )
                                       
                                       
                                       
                           )#end of tabsetpanel
                  ),#Library tabpanel end
-                 tabPanel("Original Music",)
+                 tabPanel(HTML("<h3>Original Music</h3>"),
+                          HTML("<h3>Demo 1</h3>"),
+                          uiOutput("demo1"),
+                          hr(),
+                          HTML("<h3>Demo 1 - accordion mix</h3>"),
+                          uiOutput("demo1b"),
+                          hr(),
+                          HTML("<h3>Demo 2</h3>"),
+                          uiOutput("demo2"),
+                          hr()
+                          
+                          
+                          )
 )
 
 # Define server logic required to draw a histogram
@@ -194,6 +233,74 @@ server <- function(input, output) {
              title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
              clipboard-write; encrypted-media; gyroscope; 
              picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    ####youtube####
+    output$django1 <- renderUI(
+        HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/QVAD6VPIPSs" 
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+             clipboard-write; encrypted-media; gyroscope; 
+             picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    output$bestTango <- renderUI(
+        HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/mBmGNTkr1Nk" 
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+             clipboard-write; encrypted-media; gyroscope; 
+             picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    
+    
+    output$romanticFench <- renderUI(
+        HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/pOUXNUO-oqQ" 
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+             clipboard-write; encrypted-media; gyroscope; 
+             picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    output$tangoParis <- renderUI(
+        HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/jkPX0KWxAP4" 
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+             clipboard-write; encrypted-media; gyroscope; 
+             picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    output$gotan <- renderUI(
+        HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/QybR25RPt-8" 
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+             clipboard-write; encrypted-media; gyroscope; 
+             picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    output$nakedTango <- renderUI(
+        HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/V3Tm9Hf46Iw?start=21" 
+             title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; 
+             clipboard-write; encrypted-media; gyroscope; 
+             picture-in-picture" allowfullscreen></iframe>')
+    )
+    
+    
+    
+    ####original music####
+    
+    
+    output$demo1 <- renderUI(
+        HTML('<audio controls>
+                       <source src="Ring Around Moon Demo1.mp3" type="audio/mpeg">
+                       Your browser does not support the audio element. </audio>')
+    )
+    
+    output$demo1b <- renderUI(
+        HTML('<audio controls>
+                       <source src="Ring Around Moon Demo1 Accordion Mix.mp3" type="audio/mpeg">
+                       Your browser does not support the audio element. </audio>')
+    )
+    
+    output$demo2 <- renderUI(
+        HTML('<audio controls>
+                       <source src="Ring Around Moon Demo2.mp3" type="audio/mpeg">
+                       Your browser does not support the audio element. </audio>')
     )
     
 }
